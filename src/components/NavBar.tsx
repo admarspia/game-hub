04555f56cb68ae87fdd5119142b6d0e3aa62e389
Sara <saratuemayh@gmail.com>
2025-10-12 10:56:52 +0300

    Implement Searching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,11 +4,15 @@ import logo from '../assets/Monster_game_Logo-removebg-preview.png'
 import ColorModeSwitch from './ColorModeSwitch'
 import Searchinput from './Searchinput'
 
-const NavBar = () => {
+
+interface Props {
+    onSearch: (searchText: string) => void; 
+}
+const NavBar = ({onSearch}: Props) => {
   return (
    <HStack padding={'10px'}>
     <Image src={logo} boxSize={'60px'}/>
-    <Searchinput></Searchinput>
+    <Searchinput onSearch={onSearch}></Searchinput>
     <ColorModeSwitch/>
    </HStack>
   )
